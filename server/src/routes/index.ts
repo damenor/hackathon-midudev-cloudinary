8c6f94d163c39d3f6eca0c27c3bd402420e0209f
damenor <damenor.develop@gmail.com>
2023-02-28 19:01:53 +0100

    fix puppetter

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -4,14 +4,17 @@ import puppeteer from 'puppeteer'
 export const appRouter = Router()
 
 const isValidUrl = (urlString: string) => {
-  var urlPattern = new RegExp('^(https?:\\/\\/)?'+ 
-  '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ 
-  '((\\d{1,3}\\.){3}\\d{1,3}))'+ 
-  '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ 
-  '(\\?[;&a-z\\d%_.~+=-]*)?'+ 
-  '(\\#[-a-z\\d_]*)?$','i'); 
+  var urlPattern = new RegExp(
+    '^(https?:\\/\\/)?' +
+      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' +
+      '((\\d{1,3}\\.){3}\\d{1,3}))' +
+      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' +
+      '(\\?[;&a-z\\d%_.~+=-]*)?' +
+      '(\\#[-a-z\\d_]*)?$',
+    'i'
+  )
   // const urlPattern = new RegExp(`(http)?s?:?(\/\/[^"']*\.(?:png|jpg|jpeg|gif|png|svg))`)
-  return !!urlPattern.test(urlString);
+  return !!urlPattern.test(urlString)
 }
 
 appRouter.get('/', (_req, res) => res.send('API - Auth'))
@@ -19,12 +22,22 @@ appRouter.get('/', (_req, res) => res.send('API - Auth'))
 appRouter.get('/web', async (req, res) => {
   const url = req.query.url as string
 
-  if (!url) return res.status(400).json({ error: true, message: 'La url es requerida' })
+  if (!url) return res.status(500).json({ error: true, message: 'La url es requerida' })
 
-  const browser = await puppeteer.launch({ headless: true })
+  const browserFetcher = puppeteer.createBrowserFetcher()
+  const revisionInfo = await browserFetcher.download('1095492')
+
+  if(!revisionInfo) return res.status(500).json({ error: true, message: 'ups! hubo algun error' })
+
+  const browser = await puppeteer.launch({
+    executablePath: revisionInfo.executablePath,
+    ignoreDefaultArgs: ['--disable-extensions'],
+    headless: true,
+    args: ['--no-sandbox', '--disabled-setupid-sandbox'],
+  })
   const page = await browser.newPage()
   await page.goto(url)
-  const imagesWeb = await (await page.$$eval('img', images => images.map(image => image.src)))
+  const imagesWeb = await await page.$$eval('img', images => images.map(image => image.src))
   await browser.close()
   const imagesNotDuplicates = [...new Set(imagesWeb)].filter(img => img !== '' && isValidUrl(img))
   return res.status(200).json({ images: imagesNotDuplicates })
