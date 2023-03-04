8b99e788ad3c727c0e2edb90ef2916575f3c636f
damenor <damenor.develop@gmail.com>
2023-03-04 02:11:57 +0100

    changed build

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,20 +1,8 @@
 import { config } from 'dotenv'
 
-import { appRouter } from '@/routes'
-import { Server } from '@/tools'
-// import { dbConnection } from '@utils/database.util'
-// import { initialSetupDB } from '@utils/initial-setup-db.utli'
+import { appRouter } from './routes'
+import { Server } from './tools'
 
-
-// variables enviroment
 config()
-
-// Initialize the database
-// dbConnection()
-
-// Initialize server
 const server = new Server({ router: appRouter })
 server.start()
-
-// Create defaults values DB
-// initialSetupDB()
